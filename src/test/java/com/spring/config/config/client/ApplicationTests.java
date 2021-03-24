4f896999cb46b9d3fcb2ff84167a9be81945e23f
gmaheshwari2006 <32501070+gmaheshwari2006@users.noreply.github.com>
2021-03-24 14:34:37 -0400

    init project

---
+++ src/test/java/com/spring/config/config/client/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.config.config.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
